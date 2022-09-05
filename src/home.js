c291fce0720f6246c19eefcfebfd0cad7681328f
kingraino93 <76224373+kingraino93@users.noreply.github.com>
2022-09-05 17:40:29 -0400

    final

--- src/home.js
+++ src/home.js
@@ -1,4 +1,5 @@
-import { koalas } from './koalas,js';
+export { koalas } from './koalas,js';
+
 let activeKoala = null
 
 // Changes the active koala, then re-renders the page to display that koala
@@ -6,6 +7,8 @@ let selectKoala = koala => {
     activeKoala = koala
     render()
 }
+
+
 
 // Called once when the page loads, and again every time a koala is selected
 let render = () => {
